193302f22eb426a931f5c95722d0dd38c0a07d66
Julia Saucedo <juliapaola@Julias-MacBook-Pro.local>
2017-04-11 22:57:00 -0500

    rm --cached keys.js

--- keys.js
+++
@@ -1,8 +0,0 @@
-console.log('this is loaded');
-
-exports.twitterKeys = {
-  consumer_key: 'z3OFHB9utQdp4sHjEzscMVsgG',
-  consumer_secret: 's4QaV8fQBAuTBihotrN4gPwVwjfWlUSCnUOLlRr6n3ulB81n5b',
-  access_token_key: '215169309-Q8rwmjvExDAYdD9pRxcAueCKIXCYmjI1vvZNihT9',
-  access_token_secret: 'VW87iUNWkEtWkmYajVcnQ2jrPqhP67qeKALgYPYyvXhiU',
-}
--- liri.js
+++ liri.js
@@ -2,11 +2,14 @@
 
 var command = process.argv[2];
 
-var twitter = require("./keys.js");
+var client = ("./liri.js.exports.twitterKeys");
+var twitter = require('twitter');
 
 
 if (command === "my-tweets") {
-	console.log(twitter);
+	console.log(client);
+	client.get(path, params, callback);
+
 }
 
 else if (command === "spotify-this-song") {
